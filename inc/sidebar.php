ee031528f5807883a53c9de8a2e0c2950e05f302
SALIM-LAPTOP\Salim <salim_assad@hotmail.com>
2016-11-01 21:27:38 -0600

    Actualización de los templates

---
+++ inc/sidebar.php
@@ -0,0 +1,12 @@
+<div class="col-sm-3 col-md-2 sidebar">
+    <ul class="nav nav-sidebar">
+        <li class="active"><a href="#">ITEM 1 <span class="sr-only">(current)</span></a></li>
+        <li><a href="#">ITEM 2</a></li>
+        <li><a href="#">ITEM 3</a></li>
+    </ul>
+    <ul class="nav nav-sidebar">
+        <li><a href="">ITEM 4</a></li>
+        <li><a href="">ITEM 5</a></li>
+        <li><a href="">ITEM 6</a></li>
+    </ul>
+</div>
