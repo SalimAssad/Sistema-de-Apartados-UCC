d22d9329118e39c227a74c010d71b8c02285b1c3
jaime <jaimeefox27@gmail.com>
2016-11-10 20:37:01 -0600

    Login con estilo

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
     //Tu código que necesites
 ?>
-
-
 <!DOCTYPE html>
 <html lang='es'>
     <head>
@@ -11,41 +9,36 @@
         
         <link href="css/bootstrap/css/bootstrap.min.css" rel="stylesheet">
         <link href="css/global.css" rel="stylesheet">
-        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-        <script src="login.js"></script>
         <script src="utils/jquery-1.12.3.min.js">
         </script>
     </head>
+ 
     <body bgcolor="#EEEEEE">
+
+    <br><br>
     <div align="center">
-        <div style="width: 200px; height: 170px; padding: 20px; background-color: rgba(0, 0, 0, 0); border: double">
-            <form id="form" action="Login_verify.php" method="post">
-                <label>Matrícula</label>
-                <br>
-                <input class="form-control" type="text" name="user" required>
-                <br>
-                <label>Contraseña</label>
-                <br>
-                <input class="form-control" type="password" name="password" required>
-                <br>
-                <br>
-                <input type="submit" value="Iniciar sesión">
+
+        <div style="width: 340px; height: 190px; padding: 20px; border: double">
+
+
+            <form action="Login_verify.php" method="Post">
+                
+                <label style="font-size: 14pt"><b>Matrícula:    </b></label>
+                
+                <input class="form-group has-success" style="border-radius:15px;" type="text" name="user" required>
+                
+                <label  style="font-size: 14pt"><b>Contraseña: </b></label>
+                
+                <input class="form-group has-success" style="border-radius:15px;" type="password" name="password" required>
+               <br>
+                <input class="btn btn-primary" type="submit" value="Iniciar sesión">
                 <br> <br>
-                <a href="Send_reset_email.php" >Olvidaste tu contraseña? Haz click aquí </a>
-
-
-
+                
+                <a href="inc/Send_reset_email.php" >Olvidaste tu contraseña? Haz click aquí </a>
             </form>
-
-            <br> <br>
-            <div id="error">
-                <?php
-                if(isset($_GET["error"])){
-                    echo "Los datos son incorrectos";
-                }
-                ?>
-            </div>
-
+            <div>
+               
+            </div>     
         </div>
     </div>
     </body>
