d40b378f4b2ddef4bfb725b8c64bac002669e03a
SALIM-LAPTOP\Salim <salim_assad@hotmail.com>
2016-11-14 20:25:53 -0600

    Se adaptó la lógica de inserción y actualización de areas.

--- pages/areas/saveArea.php
+++ pages/areas/saveArea.php
@@ -6,19 +6,18 @@ $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
 $campus = filter_input(INPUT_POST, 'campus', FILTER_SANITIZE_STRING);
 
 $reference = filter_input(INPUT_POST, 'reference', FILTER_SANITIZE_STRING);
+$description = filter_input(INPUT_POST, 'description', FILTER_SANITIZE_STRING);
 
 if (filter_input(INPUT_POST, 'action', FILTER_SANITIZE_STRING) == "add") {
     if ($reference == "new") {
-        $insertLocation = mysqli_query($connection, "INSERT INTO ubicaciones(UB_PILE, UB_CAMPUS, UB_FLOOR, UB_ROOM)
-                                            VALUES('$pile', '$campus','$floor','$room')");
+        $insertReference = mysqli_query($connection, "INSERT INTO referencias(RE_DESCRIPTION) VALUES('$reference')");
         $idReference = mysqli_insert_id($connection);
     } else {
         $idReference = $reference;
     }
-    $insertResource = mysqli_query($connection, "INSERT INTO recursos(RE_MODEL, RE_ALIAS, RE_TYPE, RE_AVAILABLE, RE_SERIAL, RE_INVENTORY, RE_CREATED, RE_LOCATION) 
-                                            VALUES('$model', '$name', '$type', 1, '$serial', '$inventory', NOW(), $idReference)");
+    $insertArea = mysqli_query($connection, "INSERT INTO area(AR_NAME, AR_CAMPUS, AR_TYPE) VALUES('$name', '$campus', '$reference')");
 
-    if($insertResource) {
+    if($insertArea) {
         header("Location: areas.php");
         exit;
     }else {
@@ -28,17 +27,14 @@ if (filter_input(INPUT_POST, 'action', FILTER_SANITIZE_STRING) == "add") {
 } else {    //Lógica de actualización
     $idArea = filter_input(INPUT_POST, 'idArea', FILTER_SANITIZE_NUMBER_INT);
     if ($reference == "new") {
-        $insertLocation = mysqli_query($connection, "INSERT INTO ubicaciones(UB_PILE, UB_CAMPUS, UB_FLOOR, UB_ROOM)
-                                            VALUES('$pile', '$campus','$floor','$room')");
+        $insertReference = mysqli_query($connection, "INSERT INTO referencias(RE_DESCRIPTION) VALUES('$reference')");
         $idReference = mysqli_insert_id($connection);
     } else {
         $idReference = $reference;
     }
-    $updateResource = mysqli_query($connection, "UPDATE recursos SET RE_MODEL = '$model', RE_ALIAS = '$name', RE_TYPE = '$type',
-                                             RE_SERIAL = '$serial', RE_INVENTORY = '$inventory', RE_MODIFIED = NOW(), 
-                                             RE_LOCATION = $idReference
+    $updateReference = mysqli_query($connection, "UPDATE area SET AR_NAME = '$name', AR_CAMPUS = '$campus', AR_TYPE = '$reference'
                                               WHERE RE_ID = $idArea");
-    if($updateResource) {
+    if($updateReference) {
         header("Location: areas.php");
         exit;
     }else {
