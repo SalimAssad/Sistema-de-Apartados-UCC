c837f4b35b0580cf8ff802d044490b5d20227597
Salim <salim_assad@hotmail.com>
2016-11-19 01:03:00 -0600

    -Se añadió mensaje de error.

--- pages/areas/addArea.php
+++ pages/areas/addArea.php
@@ -54,6 +54,12 @@ include_once("../../inc/nav.php");
         include_once("../../inc/sidebar.php");
         ?>
         <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+            <div class="alert alert-danger">
+                <?php
+                if (isset($_GET['error']))
+                    echo $_GET['error'];
+                ?>
+            </div>
             <form action="saveArea.php" method="post">
                 <?php
                     if(isset($id))
