e2b7580a786e459c9c478868d91a14ed35df5a54
MarcoPolo1585 <marcopolo1585@gmail.com>
2016-11-27 23:20:23 -0600

    ruta corregida.

--- pages/separate/consulta.php
+++ pages/separate/consulta.php
@@ -18,14 +18,15 @@ include_once("../../inc/MySQLConnection.php");
         <link href="../../css/dashboard.css" rel="stylesheet">
         <link href="../../css/global.css" rel="stylesheet">
         <link href="../../css/separate/separateResource.css" rel="stylesheet">
-
+        <link href="../../css/separate/consulta.css" rel="stylesheet">
+        
         <script src="../../utils/jquery-1.12.3.min.js"></script>
         <script src='../../utils/jqueryui/jquery-ui.min.js'></script>
         <script src='../../utils/moment.min.js'></script>
         <script src='../../utils/fullcalendar.min.js'></script>
         <script src="../../css/bootstrap/js/bootstrap.min.js"></script>
         <script src="../../scripts/separate/consulta.js"></script>
-        <script src="../../css/separate/consulta.css"></script>
+        
         
 
 
