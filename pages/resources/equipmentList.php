83e0fb08ce86845d5a12e71ac6458d0ef9c6f738
Salim <salim_assad@hotmail.com>
2016-12-04 14:24:08 -0600

    -Se terminó el filtrado de equipos -Identación de código

--- pages/resources/equipmentList.php
+++ pages/resources/equipmentList.php
@@ -1,124 +1,165 @@
 <?php
-//include_once("../../inc/validateLogin.php");
+include_once("../../inc/validateLogin.php");
 include_once("../../inc/MySQLConnection.php");
+include_once("resourceFunctions.php");
 
+$model = "";
+$alias = "";
+$serial = "";
+$inventory = "";
+$hwType = "";
+$campus = "";
+$reference = "";
+
+if (isset($_POST['query'])) {
+
+    if (isset($_POST['model'])) {
+        $model = filter_input(INPUT_POST, 'model', FILTER_SANITIZE_STRING);
+    }
+    if (isset($_POST['alias'])) {
+        $alias = filter_input(INPUT_POST, 'alias', FILTER_SANITIZE_STRING);
+    }
+    if (isset($_POST['serial'])) {
+        $serial = filter_input(INPUT_POST, 'serial', FILTER_SANITIZE_STRING);
+    }
+    if (isset($_POST['inventory'])) {
+        $inventory = filter_input(INPUT_POST, 'inventory', FILTER_SANITIZE_STRING);
+    }
+    if (isset($_POST['hwType'])) {
+        $hwType = filter_input(INPUT_POST, 'hwType', FILTER_SANITIZE_STRING);
+    }
+    if (isset($_POST['campus'])) {
+        $campus = filter_input(INPUT_POST, 'campus', FILTER_SANITIZE_STRING);
+    }
+    if (isset($_POST['reference'])) {
+        $reference = filter_input(INPUT_POST, 'reference', FILTER_SANITIZE_NUMBER_INT);
+    }
+
+    $resultado = getResourceQuery($connection, $model, $alias, $serial, $inventory, $hwType, $campus, $reference, "EQUIPO");
+} else {
+    $resultado = getResourceQuery($connection, "", "", "", "", "", "", "", "EQUIPO");
+}
 ?>
 <!DOCTYPE html>
 <html lang='es'>
-    <head>
-        <meta charset="utf-8">
-        <title></title>
+<head>
+    <meta charset="utf-8">
+    <title></title>
 
-        <link href="../../css/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-        <link href="../../css/dashboard.css" rel="stylesheet">
-        <link href="../../css/global.css" rel="stylesheet">
+    <link href="../../css/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../../css/dashboard.css" rel="stylesheet">
+    <link href="../../css/global.css" rel="stylesheet">
 
-        <script src="../../utils/jquery-1.12.3.min.js">
-        </script>
-        <script src="../../css/bootstrap/js/bootstrap.min.js">
-        </script>
-
-
+    <script src="../../utils/jquery-1.12.3.min.js">
+    </script>
+    <script src="../../css/bootstrap/js/bootstrap.min.js">
+    </script>
 
 
-    </head>
-    <body>
+</head>
+<body>
+<?php
+include_once("../../inc/nav.php");
+?>
+<div class="container-fluid">
+    <div class="row">
         <?php
-        include_once("../../inc/nav.php");
+        include_once("../../inc/sidebar.php");
         ?>
-        <div class="container-fluid">
-            <div class="row">
-                <?php
-                include_once("../../inc/sidebar.php");
-                ?>
-                <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-                    <h2 class="page-header">Recursos</h2>
+        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+            <h2 class="page-header">Recursos</h2>
+
+            <form action="equipmentList.php" method="post">
 
 
-                 <form action="addResource.php" method="get" >   <button type="submit" formaction="addResource.php" class="btn btn-primary" method="get">Añadir </button></form>
+                modelo <br>
+                <input type="text" name="model" value="<?php echo $model; ?>">
+                <br> Alias <br>
+                <input type="text" name="alias" value="<?php echo $alias; ?>">
+                <br> serial<br>
+                <input type="text" name="serial" value="<?php echo $serial; ?>">
+                <br> No. Inventario <br>
+                <input type="text" name="inventory" value="<?php echo $inventory; ?>">
+                <br> Tipo Hardware <br>
+                <input type="text" name="hwType" value="<?php echo $hwType; ?>">
+                <br> campus<br>
+
+                <select name="campus">
+                    <option value="">Seleccione</option>
+                    <option value="TORRENTE" <?php if ($campus == "TORRENTE") echo "selected"; ?>>Torrente
+                    </option>
+                    <option value="CALASANZ" <?php if ($campus == "CALASANZ") echo "selected"; ?>>Calasanz
+                    </option>
+
+                </select>
+                <br> Referencia<br>
+                <select name="reference">
+                    <option value="">Seleccione</option>
+                    <?php
+                    $referenceQuery = mysqli_query($connection, "SELECT * FROM referencias");
+                    while ($referenceRow = mysqli_fetch_assoc($referenceQuery)) {
+                        echo "<option value='$referenceRow[RE_ID]'";
+                        if ($referenceRow['RE_ID'] == $reference)
+                            echo " selected";
+                        echo ">$referenceRow[RE_DESCRIPTION]</option>";
+                    }
+                    ?>
+                </select>
+
+                <input type="submit" name="query" value="buscar">
+
+            </form>
+
+            <form action="addResource.php" method="get">
+                <button type="submit" formaction="addResource.php" class="btn btn-primary" method="get">Añadir</button>
+            </form>
 
 
-                    <?php 
-                    $sql="select * from recursos";
-                    $resultado = mysqli_query($connection, $sql);
-                    if($resultado){
-                        $cantidad_recursos=mysqli_num_rows($resultado); 
+            <?php
+            if ($resultado) {
+                $cantidad_recursos = mysqli_num_rows($resultado);
 
-                        if($cantidad_recursos > 0){
+                if ($cantidad_recursos > 0) {
                     ?>
 
-
-<form action="equipmentList.php" method="post"> 
-    
-   
-    
-    
-                 modelo <br>   
-                  <input type="text"  name="model" value= "">
-                <br> Alias <br>
-                   <input type="text" name="alias" value= "">
-            <br> serial<br>
-                    <input type="text" name="serial" value="">
-    <br> No. Inventario <br>   
-                  <input type="text"  name="inventory" value= "">
-                <br> Tipo Hardware <br>
-                   <input type="text" name="hwtype" value= "">
-            <br> campus<br>
-          
-                        <select  name="campus">
-                                                <option value="">Seleccione</option>
-                                                <option value="TORRENTE">Torrente</option>
-                            <option value="CALASANZ">Calasanz</option>
-                    
-                    </select>
-    <br> Referencia<br>
-                    <input type="text" name="reference" value="">
-                
-                <input type="submit" value="buscar">
-                
-                </form>
-                    
-                    
-                    
-                    
-                    
 
                     <table class="table">
                         <thead>
-                            <tr>
-                                <th>Nombre del equipo</th>
-                                <th>Marca/Modelo</th>
-                                <th>No. de Serie</th>
-                                <th> No. de Inventario</th>
-                                <th> Asignado en</th>
-                                <th> Opciones</th>
-                            </tr>
-                        </thead>
-                    <tbody>
-                        <?php 
-                            while($fila = mysqli_fetch_assoc($resultado)){
-                            ?>
-                        
-                            <td><?php echo $fila["RE_ALIAS"]?></td>
-                            <td><?php echo $fila["RE_MODEL"]?></td>
-                            <td><?php echo $fila["RE_SERIAL"]?></td>
-                            <td><?php echo $fila["RE_INVENTORY"]?></td>
-                            <td><?php echo $fila["RE_LOCATION"]?></td>
-                            <td> <a href="specificEquipment.php=<?php echo $fila["RE_ID"] ?>"><button type="button" class="btn btn-success">Ver detalles</button>    </td>
+                        <tr>
+                            <th>Nombre del equipo</th>
+                            <th>Marca/Modelo</th>
+                            <th>No. de Serie</th>
+                            <th> No. de Inventario</th>
+                            <th> Asignado en</th>
+                            <th> Opciones</th>
                         </tr>
+                        </thead>
+                        <tbody>
+                        <?php
+                        while ($fila = mysqli_fetch_assoc($resultado)) {
+                            ?>
+
+                            <td><?php echo $fila["RE_ALIAS"] ?></td>
+                            <td><?php echo $fila["RE_MODEL"] ?></td>
+                            <td><?php echo $fila["RE_SERIAL"] ?></td>
+                            <td><?php echo $fila["RE_INVENTORY"] ?></td>
+                            <td><?php echo $fila["RE_LOCATION"] ?></td>
+                            <td><a href="specificEquipment.php=<?php echo $fila["RE_ID"] ?>">
+                                    <button type="button" class="btn btn-success">Ver detalles</button></td>
+                            </tr>
                         <?php }; ?>
-                    
-                    </tbody>
-                </table>
-                <?php
-                        }else{
-                            echo "No hay recursos registrados";
-                        }
-                    }
-                    ?>
-        </div>
+
+                        </tbody>
+                    </table>
+                    <?php
+                } else {
+                    echo "No hay recursos registrados";
+                }
+            }
+            ?>
         </div>
     </div>
+</div>
 </div>
 </body>
 </html>
--- pages/resources/roomList.php
+++ pages/resources/roomList.php
@@ -7,138 +7,138 @@ $alias = "";
 $campus = "";
 $reference = "";
 
-if(isset($_POST['query'])){
+if (isset($_POST['query'])) {
 
-    if(isset($_POST['alias'])){
+    if (isset($_POST['alias'])) {
         $alias = filter_input(INPUT_POST, 'alias', FILTER_SANITIZE_STRING);
     }
-    if(isset($_POST['campus'])){
+    if (isset($_POST['campus'])) {
         $campus = filter_input(INPUT_POST, 'campus', FILTER_SANITIZE_STRING);
     }
-    if(isset($_POST['reference'])){
+    if (isset($_POST['reference'])) {
         $reference = filter_input(INPUT_POST, 'reference', FILTER_SANITIZE_NUMBER_INT);
     }
 
 
-    $resultado = getResourceQuery($connection,"",$alias,"","","",$campus,$reference,"AULA");
-}else{
-    $resultado = getResourceQuery($connection,"","","","","","","","AULA");
+    $resultado = getResourceQuery($connection, "", $alias, "", "", "", $campus, $reference, "AULA");
+} else {
+    $resultado = getResourceQuery($connection, "", "", "", "", "", "", "", "AULA");
 }
 
 ?>
 <!DOCTYPE html>
 <html lang='es'>
-    <head>
-        <meta charset="utf-8">
-        <title>Eliminar un salón></title>
+<head>
+    <meta charset="utf-8">
+    <title>Eliminar un salón></title>
 
-        <link href="../../css/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-        <link href="../../css/dashboard.css" rel="stylesheet">
-        <link href="../../css/global.css" rel="stylesheet">
+    <link href="../../css/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../../css/dashboard.css" rel="stylesheet">
+    <link href="../../css/global.css" rel="stylesheet">
 
-        <script src="../../utils/jquery-1.12.3.min.js">
-        </script>
-        <script src="../../css/bootstrap/js/bootstrap.min.js">
-        </script>
-
-
+    <script src="../../utils/jquery-1.12.3.min.js">
+    </script>
+    <script src="../../css/bootstrap/js/bootstrap.min.js">
+    </script>
 
 
-    </head>
-    <body>
+</head>
+<body>
+<?php
+include_once("../../inc/nav.php");
+?>
+<div class="container-fluid">
+    <div class="row">
         <?php
-        include_once("../../inc/nav.php");
+        include_once("../../inc/sidebar.php");
         ?>
-        <div class="container-fluid">
-            <div class="row">
-                <?php
-                include_once("../../inc/sidebar.php");
-                ?>
-                <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-                    <h2 class="page-header">Lista de recursos</h2>
+        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+            <h2 class="page-header">Lista de recursos</h2>
 
 
-                 <form action="addResource.php" method="get" >   <button type="submit" formaction="addResource.php" class="btn btn-primary" method="get">Dar de alta </button></form>
+            <form action="addResource.php" method="get">
+                <button type="submit" formaction="addResource.php" class="btn btn-primary" method="get">Dar de alta
+                </button>
+            </form>
 
 
-                    <?php
-                    if($resultado){
-                        $cantidad_recursos=mysqli_num_rows($resultado);
+            <?php
+            if ($resultado) {
+                $cantidad_recursos = mysqli_num_rows($resultado);
 
-                        if($cantidad_recursos > 0){
+                if ($cantidad_recursos > 0) {
                     ?>
-
-
 
 
                     <table class="table">
                         <thead>
-                            <tr>
-                                <th>Nombre del equipo</th>
-                                <th>Marca/Modelo</th>
-                                <th>No. de Serie</th>
-                                <th> No. de Inventario</th>
-                                <th> Asignado en</th>
-                                <th> Opciones</th>
-                            </tr>
-                        </thead>
-                  <tbody>
-                        <?php 
-                            while($fila = mysqli_fetch_assoc($resultado)){
-                            ?>
-                        
-                            <td><?php echo $fila["RE_ALIAS"]?></td>
-                        <td><?php echo $fila["RE_MODEL"]?></td>
-                            <td><?php echo $fila["RE_SERIAL"]?></td>
-                            <td><?php echo $fila["RE_INVENTORY"]?></td>
-                            <td><?php echo $fila["RE_LOCATION"]?></td>
-                            <td> <a href="specificEquipment.php=<?php echo $fila["RE_ID"] ?>"><button type="button" class="btn btn-success">Ver detalles</button>    </td>
+                        <tr>
+                            <th>Nombre del equipo</th>
+                            <th>Marca/Modelo</th>
+                            <th>No. de Serie</th>
+                            <th> No. de Inventario</th>
+                            <th> Asignado en</th>
+                            <th> Opciones</th>
                         </tr>
+                        </thead>
+                        <tbody>
+                        <?php
+                        while ($fila = mysqli_fetch_assoc($resultado)) {
+                            ?>
+
+                            <td><?php echo $fila["RE_ALIAS"] ?></td>
+                            <td><?php echo $fila["RE_MODEL"] ?></td>
+                            <td><?php echo $fila["RE_SERIAL"] ?></td>
+                            <td><?php echo $fila["RE_INVENTORY"] ?></td>
+                            <td><?php echo $fila["RE_LOCATION"] ?></td>
+                            <td><a href="specificEquipment.php=<?php echo $fila["RE_ID"] ?>">
+                                    <button type="button" class="btn btn-success">Ver detalles</button></td>
+                            </tr>
                         <?php }; ?>
-                    
-                    </tbody>
-                </table>
-                <?php
-                        }else{
-                            echo "No hay recursos registrados";
-                        }
+
+                        </tbody>
+                    </table>
+                    <?php
+                } else {
+                    echo "No hay recursos registrados";
+                }
+            }
+            ?>
+
+
+            <form action="roomList.php" method="post">
+                Alias <br>
+                <input type="text" name="alias" value="<?php echo $alias; ?>">
+                <br> Campus <br>
+                <select name="campus">
+                    <option value="">Seleccione</option>
+                    <option value="TORRENTE" <?php if ($campus == "TORRENTE") echo "selected"; ?>>Torrente</option>
+                    <option value="CALASANZ" <?php if ($campus == "CALASANZ") echo "selected"; ?>>Calasanz</option>
+
+                </select>
+                <br> Referencia<br>
+                <select name="reference">
+                    <option value="">Seleccione</option>
+                    <?php
+                    $referenceQuery = mysqli_query($connection, "SELECT * FROM referencias");
+                    while ($referenceRow = mysqli_fetch_assoc($referenceQuery)) {
+                        echo "<option value='$referenceRow[RE_ID]'";
+                        if ($referenceRow['RE_ID'] == $reference)
+                            echo " selected";
+                        echo ">$referenceRow[RE_DESCRIPTION]</option>";
                     }
                     ?>
-                
-                
-                <form action="roomList.php" method="post">
-                 Alias <br>   
-                  <input type="text"  name="alias" value= "<?php echo $alias; ?>">
-                <br> Campus <br>
-                    <select name="campus">
-                            <option value="">Seleccione</option>
-                            <option value="TORRENTE" <?php if($campus == "TORRENTE") echo "selected"; ?>>Torrente</option>
-                            <option value="CALASANZ" <?php if($campus == "CALASANZ") echo "selected"; ?>>Calasanz</option>
-            
-                    </select>
-                        <br> Referencia<br>
-                    <select name="reference">
-                        <option value="">Seleccione</option>
-                        <?php
-                            $referenceQuery = mysqli_query($connection, "SELECT * FROM referencias");
-                            while ($referenceRow = mysqli_fetch_assoc($referenceQuery)){
-                                echo "<option value='$referenceRow[RE_ID]'";
-                                if($referenceRow['RE_ID'] == $reference)
-                                    echo " selected";
-                                echo ">$referenceRow[RE_DESCRIPTION]</option>";
-                            }
-                        ?>
-                    </select>
-                    <br>
-               <br>  <input type="submit" name="query" value="Buscar">
-                
-                
-                </form>
-                
-                
-        </div>
+                </select>
+                <br>
+                <br> <input type="submit" name="query" value="Buscar">
+
+
+            </form>
+
+
         </div>
     </div>
+</div>
 </div>
 </body>
 </html>
