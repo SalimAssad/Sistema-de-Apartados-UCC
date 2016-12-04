42331e0b796f0d85a814e76aa00ed3c8139954ac
Salim <salim_assad@hotmail.com>
2016-12-04 14:35:09 -0600

    -Se terminó el filtrado de area

--- pages/resources/areaList.php
+++ pages/resources/areaList.php
@@ -1,129 +1,142 @@
 <?php
-//include_once("../../inc/validateLogin.php");
+include_once("../../inc/validateLogin.php");
 include_once("../../inc/MySQLConnection.php");
-//Tu código que necesites
+include_once("resourceFunctions.php");
+
+$name = "";
+$campus = "";
+$reference = "";
+
+if (isset($_POST['query'])) {
+
+    if (isset($_POST['name'])) {
+        $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
+    }
+    if (isset($_POST['campus'])) {
+        $campus = filter_input(INPUT_POST, 'campus', FILTER_SANITIZE_STRING);
+    }
+    if (isset($_POST['reference'])) {
+        $reference = filter_input(INPUT_POST, 'reference', FILTER_SANITIZE_NUMBER_INT);
+    }
+
+
+    $resultado = getAreaQuery($connection, $name, $campus, $reference);
+} else {
+    $resultado = getAreaQuery($connection, "", "", "");
+}
 ?>
 <!DOCTYPE html>
 <html lang='es'>
-    <head>
-        <meta charset="utf-8">
-        <title>Eliminar un salón></title>
+<head>
+    <meta charset="utf-8">
+    <title>Eliminar un salón></title>
 
-        <link href="../../css/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-        <link href="../../css/dashboard.css" rel="stylesheet">
-        <link href="../../css/global.css" rel="stylesheet">
+    <link href="../../css/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../../css/dashboard.css" rel="stylesheet">
+    <link href="../../css/global.css" rel="stylesheet">
 
-        <script src="../../utils/jquery-1.12.3.min.js">
-        </script>
-        <script src="../../css/bootstrap/js/bootstrap.min.js">
-        </script>
-        
-        
-   
-        
-    </head>
-    <body>
+    <script src="../../utils/jquery-1.12.3.min.js">
+    </script>
+    <script src="../../css/bootstrap/js/bootstrap.min.js">
+    </script>
+
+
+</head>
+<body>
+<?php
+include_once("../../inc/nav.php");
+?>
+<div class="container-fluid">
+    <div class="row">
         <?php
-        include_once("../../inc/nav.php");
+        include_once("../../inc/sidebar.php");
         ?>
-        <div class="container-fluid">
-            <div class="row">
-                <?php
-                include_once("../../inc/sidebar.php");
-                ?>
-                <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-                    <h1 class="page-header"></h1>
+        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+            <h1 class="page-header"></h1>
 
 
-                    <h3 class="sub-header">Área</h3>
-                    
-                    <button type="button" class="btn btn-primary">Dar de alta</button>
-                    <table class="table">
-                        
-                        /////////////////////////////////////////////////////////////
-                        
-                        <form action="areaList.php" method="post"> 
-    
-   
-    
-    
-                 Name <br>   
-                  <input type="text"  name="name" value= "">
-                
-                 <br> campus<br>
-          
-                 <select name="campus">
-                                            <option value="">Seleccione</option>
-                                            <option value="TORRENTE">Torrente</option>
-                                            <option value="CALASANZ">Calasanz</option>
-                    
-                    </select>
-    <br> Referencia<br>
-                    <input type="text" name="reference" value="">
-                
-                <input type="submit" value="buscar">
-                
-                </form>
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-                        /////////////////////////////////////////////////////////////////////
-                        
-                        <?php 
-                    $sql="select * from areas";
-                    $resultado = mysqli_query($connection, $sql);
-                    if($resultado){
-                        $cantidad_areas=mysqli_num_rows($resultado); 
+            <h3 class="sub-header">Área</h3>
+            /////////////////////////////////////////////////////////////
 
-                        if($cantidad_areas > 0){
-                    ?>
+            <form action="areaList.php" method="post">
 
-            <table class="table">
-    <thead>
-      <tr>
-       <th>Nombre</th>
-        <th>Campus</th>
-        <th>Referencias</th>
-          <th> Opciones</th>
-         
-      </tr>
-    </thead>
-    <tbody>
-        <?php 
-                            while($fila = mysqli_fetch_assoc($resultado)){
-                            ?>
-        
-      <td><?php echo $fila["AR_NAME"]?></td>
-        <td><?php echo $fila["AR_CAMPUS"]?> </td>
-        <td> <?php echo $fila["AR_SID"]?> </td>
-          <td> 
-  
-  
-  </tr>
- <?php }; ?>
-                </tbody>
-                    </table>
+
+                Name <br>
+                <input type="text" name="name" value="<?php echo $name; ?>">
+
+                <br> campus<br>
+
+                <select name="campus">
+                    <option value="">Seleccione</option>
+                    <option value="TORRENTE" <?php if ($campus == "TORRENTE") echo "selected"; ?>>Torrente</option>
+                    <option value="CALASANZ" <?php if ($campus == "CALASANZ") echo "selected"; ?>>Calasanz</option>
+
+                </select>
+                <br> Referencia<br>
+                <select name="reference">
+                    <option value="">Seleccione</option>
                     <?php
-                        }else{
-                            echo "No hay recursos registrados";
-                        }
+                    $referenceQuery = mysqli_query($connection, "SELECT * FROM referencias");
+                    while ($referenceRow = mysqli_fetch_assoc($referenceQuery)) {
+                        echo "<option value='$referenceRow[RE_ID]'";
+                        if ($referenceRow['RE_ID'] == $reference)
+                            echo " selected";
+                        echo ">$referenceRow[RE_DESCRIPTION]</option>";
                     }
-                        
                     ?>
-</div>
-                </div>
-            </div>
+                </select>
+
+                <input type="submit" name="query" value="buscar">
+
+            </form>
+
+
+            /////////////////////////////////////////////////////////////////////
+
+            <button type="button" class="btn btn-primary">Dar de alta</button>
+            <table class="table">
+
+                <?php
+                if ($resultado) {
+                    $cantidad_areas = mysqli_num_rows($resultado);
+
+                    if ($cantidad_areas > 0) {
+                        ?>
+
+                        <table class="table">
+                            <thead>
+                            <tr>
+                                <th>Nombre</th>
+                                <th>Campus</th>
+                                <th>Referencias</th>
+                                <th> Opciones</th>
+
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <?php
+                            while ($fila = mysqli_fetch_assoc($resultado)) {
+                                ?>
+
+                                <td><?php echo $fila["AR_NAME"] ?></td>
+                                <td><?php echo $fila["AR_CAMPUS"] ?> </td>
+                                <td> <?php echo $fila["AR_SID"] ?> </td>
+
+
+                                </tr>
+                            <?php }; ?>
+                            </tbody>
+                        </table>
+                        <?php
+                    } else {
+                        echo "No hay recursos registrados";
+                    }
+                }
+
+                ?>
         </div>
-    </body>
+    </div>
+</div>
+</div>
+</body>
 </html>
