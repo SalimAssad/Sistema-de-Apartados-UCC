e2fadf172e53abd3089cfb01af264e28dee521ef
MarcoPolo1585 <marcopolo1585@gmail.com>
2016-12-06 02:42:13 -0600

    lukadis!!!

--- scripts/separate/ajax/cancelSeparate.php
+++ scripts/separate/ajax/cancelSeparate.php
@@ -9,31 +9,27 @@ $motivo = $_POST["motivo"];
 
 include_once("../../inc/MySQLConnection.php");
 
-
-
-
-
-
-if(!query){
-    exit("false: bad query");
-}
-
-
-
-
 if($authCode == $codigo){
     $update = "UPDATE apartados  SET AP_CANCEL = 1 WHERE AP_ID = '$id'";
-$query = mysqli_query($connection,$update);
-    
-    $query = mysqli_query($connection,$consulta);
-    
+    $query = mysqli_query($connection,$update);
+
+    if(!$query){
+        exit("false: bad query");
+    }
+
+
+
     $insert = "INSERT INTO  apartados_cancelados (AC_SEPARATEID,AC_RESPONSIBLE,AC_DATE,AC_REASON) VALUES('$id','$user_id',NOW(),'$motivo'); ";
-    
+
     $query2 = mysqli_query($connection,$insert);
-    
+
+    if(!query2){
+        exit("false: bad query");
+    }
+
     echo query;
     echo query2;
-        
+
 }else{
     exit("FALSE");
 }
