90307dc7c49cb6085556146b590f91100d4a3f05
MarcoPolo1585 <marcopolo1585@gmail.com>
2016-12-06 02:37:34 -0600

    !!!!!!

--- scripts/separate/ajax/cancelSeparate.php
+++ scripts/separate/ajax/cancelSeparate.php
@@ -12,25 +12,30 @@ include_once("../../inc/MySQLConnection.php");
 
 
 
-$update = "UPDATE apartados  SET AP_CANCEL = 1 WHERE AP_ID = '$id'";
-$query = mysqli_query($connection,$update);
+
 
 if(!query){
     exit("false: bad query");
 }
 
-$row = mysqli_fetch_assoc($query);
+
 
 
 if($authCode == $codigo){
+    $update = "UPDATE apartados  SET AP_CANCEL = 1 WHERE AP_ID = '$id'";
+$query = mysqli_query($connection,$update);
+    
     $query = mysqli_query($connection,$consulta);
     
     $insert = "INSERT INTO  apartados_cancelados (AC_SEPARATEID,AC_RESPONSIBLE,AC_DATE,AC_REASON) VALUES('$id','$user_id',NOW(),'$motivo'); ";
     
     $query2 = mysqli_query($connection,$insert);
+    
+    echo query;
+    echo query2;
         
 }else{
-    exit();
+    exit("FALSE");
 }
 
 
